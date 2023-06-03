c49c3a6259a879673f92704109ac2f4a7ba4319b
Ryednap <ujjwalpandey408@gmail.com>
2023-06-03 13:08:22 +0530

    03-06-2023 13:28 | Pre softmax solution commit

--- assignment1/src/classifiers/softmax.py
+++ assignment1/src/classifiers/softmax.py
@@ -3,11 +3,46 @@ from typing import Tuple
 import numpy as np
 
 
+def softmax_loss_naive(W: np.ndarray, X: np.ndarray, y: np.ndarray, reg: float) -> Tuple[float, np.ndarray]:
+    """
+    This method contains Softmax loss function, naive implementation (with loops)
+
+    Inputs have dimension D, there are C classes, and we operate on mini-batches of N examples.
+
+    :param W: A numpy array of shape (D, C) containing weights.
+    :param X: A numpy array of shape (N, D) containing inputs.
+    :param y: A numpy array of shape (N,) containing labels;
+        y[i] = c means that X[i] has label c, where 0 <= c < C.
+    :param reg: A floating number representing regularization strength.
+    :return: A tuple of:
+        - loss as single float
+        - A numpy array of gradient with respect to weight W; an array of shape as W.
+    """
+
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    pass
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    return loss, dW
+
+
 def softmax_loss_vectorized(W: np.ndarray, X: np.ndarray, y: np.ndarray, reg: float) -> Tuple[float, np.ndarray]:
     """
-    Structured SVM loss function, vectorized implementation (optimal).
+    This method contains Softmax loss function, vectorized implementation (optimal).
 
-    Inputs and outputs are the same as `svm_loss_naive`.
+    Inputs and outputs are the same as `softmax_loss_naive`.
 
     :param W: A numpy array of shape (D, C) containing weights.
     :param X: A numpy array of shape (N, D) containing a mini-batch of data.
@@ -19,5 +54,21 @@ def softmax_loss_vectorized(W: np.ndarray, X: np.ndarray, y: np.ndarray, reg: fl
         - loss as single float
         - gradient with respect to weights W; a numpy array of the same shape as W.
     """
+
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
     pass
 
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    return loss, dW
